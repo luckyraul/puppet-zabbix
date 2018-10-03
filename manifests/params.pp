f2422e294253ee1941f366b78abe1fd356d91461
Nikita <luckyraul@users.noreply.github.com>
2018-10-04 00:55:20 +0300

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 # == Class: nginx::params
 class zabbix::params {
-    $version = '3.4'
+    $version = '4.0'
     $agent_ensure = 'latest'
     $agent_service_ensure = 'running'
     $agent_service_enable = true
