de9c8621321cc29955ad30e7e6c5776db2ec84c5
Nikita <luckyraul@users.noreply.github.com>
2021-05-21 23:49:54 +0300

    5.4

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 # == Class: zabbix::params
 class zabbix::params {
-    $version = '5.2'
+    $version = '5.4'
     $agent_ensure = 'latest'
     $agent_service_ensure = 'running'
     $agent_service_enable = true
