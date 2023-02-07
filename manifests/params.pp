8c173a0c1efcb6b97191ca91419ff87f9057e0d4
Nikita <luckyraul@users.noreply.github.com>
2023-02-07 13:16:30 +0300

    6.2 by default

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 # == Class: zabbix::params
 class zabbix::params {
-    $version = '5.4'
+    $version = '6.2'
     $agent_ensure = 'latest'
     $agent_service_ensure = 'running'
     $agent_service_enable = true
